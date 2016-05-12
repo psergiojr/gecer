c479ee47ec4485f937a36eba12219fb2d155ad89
Paulo Sérgio Jr <paulo.psw@gmail.com>
2016-05-12 19:01:55 -0300

    Added Apache License 2 (GPL-compatible), fixes #2

--- gecer.py
+++ gecer.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python3
+# GeCer v0.1
+#     Gerador de Certificados
+#
+# Copyright 2016 - Paulo Sérgio Jr <paulo.psw@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from subprocess import call
 
 # Endereço do arquivo de nomes:
