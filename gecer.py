84ed4d2fd175e42f20edbfa1d5e29f9afaf29e46
Paulo Sérgio Jr <paulo.psw@gmail.com>
2016-05-14 10:07:16 -0300

    Update comments

--- gecer.py
+++ gecer.py
@@ -25,11 +25,11 @@ arq_nomes = 'nomes.txt'
 # Endereço do template SVG:
 arq_template = 'template.svg'
 
-# Lendo o arquivo para uma string de template
+# Lendo o arquivo de template
 with open(arq_template, encoding='utf-8') as tmp:
     template = tmp.read()
 
-
+# Lendo o arquivo de nomes
 with open(arq_nomes, encoding='utf-8') as nomes:
     for nome in nomes:
         # Ignora linhas de separação ou em branco
